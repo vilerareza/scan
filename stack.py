9f517e8e97ae70d252dba04053c16f0e8b220a71
Reza Vilera <vilera_reza99@yahoo.com>
2022-06-20 23:23:51 +0700

    test

--- stack.py
+++ stack.py
@@ -6,6 +6,23 @@ import pickle
 
 imgDir = "img_corrected"
 
+def thresholding(img, threshold=50):
+    it = np.nditer(img, flags = ['multi_index'], op_flags = ['readwrite']) #iterator
+    for x in it:
+        if (x < threshold):
+            x[...] = 0
+        else:
+            x[...] = 255
+    return img
+
+def find_tray_edge (edge_pixels):
+    # Return the estimated tray position: transition between 0 (black) to 255 (white)
+    i = 0    
+    for pixel in edge_pixels:
+        if pixel == 255:
+            return i
+        i+=1
+
 def shift_image(img, shift):
     if shift < 0:
         # Shift right
@@ -30,24 +47,43 @@ def stack_image(imgs = []):
 
 def align_and_stack(imgDir):
     try:
-        shifts = pickle.load(open('alignment_shifts.p',"rb"))
         imageFiles = os.listdir(imgDir)
         if len(imageFiles) > 0:
             imgs = []
+            tray_edges = []
             for i in range(len(imageFiles)):
                 filePath = os.path.join(imgDir, f'{i}.png')
                 img = cv.imread(filePath)
                 imgs.append(img)
+            # Find the image edges and threshold it to 0 or 255
+                if (i % 2):
+                    # If even image the top edge
+                    edge = thresholding(cv.cvtColor(img, cv.COLOR_BGR2GRAY)[0,::], threshold = 50)
+                else:
+                    # Edd image take the bottom edge
+                    edge = thresholding(cv.cvtColor(img, cv.COLOR_BGR2GRAY)[-1,::], threshold = 50)
+                # Find tray edge
+                tray_edge = find_tray_edge(edge)
+                tray_edges.append(tray_edge)
+            
+            if len(tray_edges) > 1:
+                shifts = []
+                for i in range(len(tray_edges)-1):
+                    shift = tray_edges[i+1]-tray_edges[i]
+                    shifts.append(shift)
+                #Accumulate the shift
+                for i in range(len(shifts)-1):
+                    shifts[i+1] = shifts[i+1]+shifts[i]
+            # Shift the images (except image 0)
             for i in range(len(imgs)):
                 if i > 0:
                     imgs[i]=shift_image(imgs[i], shifts[i-1]).astype('uint8')
-
             # Output
-        #     outimg = stack_image(imgs)
-        #     cv.imwrite('Scan.png', outimg)
-        #     return True
-        # else:
-        #     return False
+            outimg = stack_image(imgs)
+            cv.imwrite('Scan.png', outimg)
+            return True
+        else:
+            return False
 
     except Exception as e:
         print (e)
