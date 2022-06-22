57a6f8d4ba05632bf0bca7b546a7e45694dc8804
Reza Vilera <vilera_reza99@yahoo.com>
2022-06-22 21:59:03 +0700

    increase threshold

--- stack.py
+++ stack.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 
 imgDir = "img_corrected"
+threshold = 75
 
 def thresholding(img, threshold=50):
     it = np.nditer(img, flags = ['multi_index'], op_flags = ['readwrite']) #iterator
@@ -44,7 +45,7 @@ def stack_image(imgs = []):
     # Stack all the images in the array
     return np.vstack([img for img in imgs][::-1])
 
-def align_and_stack(imgDir):
+def align_and_stack(imgDir, threshold):
     try:
         imageFiles = os.listdir(imgDir)
         if len(imageFiles) > 0:
@@ -56,8 +57,8 @@ def align_and_stack(imgDir):
                 img = cv.imread(filePath).astype('uint8')
                 imgs.append(img.astype('uint8'))
                 # Find the image edges and threshold it to 0 or 255
-                edge_t = thresholding(cv.cvtColor(img, cv.COLOR_BGR2GRAY)[0,::])
-                edge_b = thresholding(cv.cvtColor(img, cv.COLOR_BGR2GRAY)[-1,::])
+                edge_t = thresholding(cv.cvtColor(img, cv.COLOR_BGR2GRAY)[0,::], threshold)
+                edge_b = thresholding(cv.cvtColor(img, cv.COLOR_BGR2GRAY)[-1,::], threshold)
                 tray_edge_t = find_tray_edge(edge_t)
                 tray_edge_b = find_tray_edge(edge_b)
                 tray_edges_t.append(tray_edge_t)
@@ -87,8 +88,10 @@ def align_and_stack(imgDir):
         return []
 
 def post_process ():
+    global imgDir
+    global threshold
     t1 = time.time()
-    outImg = align_and_stack(imgDir)
+    outImg = align_and_stack(imgDir, threshold)
     outFileName = f'Scan_{time.strftime("%Y-%m-%d_%H-%M-%S")}.png'
     if len(outImg) > 0:
         cv.imwrite(outFileName, outImg)
